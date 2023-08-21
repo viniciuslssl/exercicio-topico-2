28e9d59f2f343d44137b9b43ad727edb3177af2c
viniciuslssl <142353095+viniciuslssl@users.noreply.github.com>
2023-08-21 20:55:18 -0300

    Add files via upload

---
+++ topico2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main(){
+    int n;
+    int dobro;
+
+    printf("Digite um numero e veja o dobro");
+    scanf("%d",&n);
+    dobro = n * 2;
+    printf("Resultado: %d ",dobro);
+}
